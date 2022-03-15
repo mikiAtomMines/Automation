86bbb1851909855cca5e16d48440d6e5b9eadc11
Sebastian Miki-Silva <miki@atommines.com>
2022-03-15 18:03:53 -0500

    Added TODO list.

--- PowerSupplyClass.py
+++ PowerSupplyClass.py
@@ -4,10 +4,13 @@ Created on Fri Mar 11 12:53:39 2022
 @author: Sebastian Miki-Silva
 """
 
-import socket # for sockets
+#TODO: fix usbtmc power supply class.
 
-import sys # for exit
-import time # for sleep
+import socket
+import usbtmc
+import usb
+import sys
+import time
 
 
 class EthernetPowerSupply:
@@ -254,15 +257,22 @@ class UsbtmcPowerSupply:
     def __init__(self, id_vendor=None, id_product=None, MAX_voltage=1000, MAX_current=100, reset_channels=True):
         self._id_vendor = id_vendor
         self._id_product = id_product
-        self._MAX_voltage = MAX_voltage
-        self._MAX_current = MAX_current
-        self._ch1_voltage_limit = MAX_voltage
-        self._ch1_current_limit = MAX_current
-        self._ch2_voltage_limit = MAX_voltage
-        self._ch2_current_limit = MAX_current
+        # self._MAX_voltage = MAX_voltage
+        # self._MAX_current = MAX_current
+        # self._ch1_voltage_limit = MAX_voltage
+        # self._ch1_current_limit = MAX_current
+        # self._ch2_voltage_limit = MAX_voltage
+        # self._ch2_current_limit = MAX_current
+
+        self._instrument = usbtmc.Instrument(id_vendor, id_product)
+
+    def __query(self, query):
+        reply = self._instrument.ask('*IDN?')
+        return reply
 
     
-def main():
+def testing_EthernetPowerSupply():
+
     SPD3303X = EthernetPowerSupply('10.176.42.121', 5025, MAX_voltage=32, MAX_current=3.2)
     print(SPD3303X.idn)
     print(SPD3303X.ip4_address)
@@ -313,6 +323,77 @@ def main():
     SPD3303X.reset_channels()
     SPD3303X.disconnect()
 
+def testing_UsbtmcPowerSupply():
+    SPD3303X = UsbtmcPowerSupply(id_vendor=0xF4EC, id_product=0x1430)
+
+def main():
+    USBTMC_bInterfaceClass = 0xFE
+    USBTMC_bInterfaceSubClass = 3
+    USBTMC_bInterfaceProtocol = 0
+    USB488_bInterfaceProtocol = 1
+
+    USBTMC_MSGID_DEV_DEP_MSG_OUT = 1
+    USBTMC_MSGID_REQUEST_DEV_DEP_MSG_IN = 2
+    USBTMC_MSGID_DEV_DEP_MSG_IN = 2
+    USBTMC_MSGID_VENDOR_SPECIFIC_OUT = 126
+    USBTMC_MSGID_REQUEST_VENDOR_SPECIFIC_IN = 127
+    USBTMC_MSGID_VENDOR_SPECIFIC_IN = 127
+    USB488_MSGID_TRIGGER = 128
+
+    USBTMC_STATUS_SUCCESS = 0x01
+    USBTMC_STATUS_PENDING = 0x02
+    USBTMC_STATUS_FAILED = 0x80
+    USBTMC_STATUS_TRANSFER_NOT_IN_PROGRESS = 0x81
+    USBTMC_STATUS_SPLIT_NOT_IN_PROGRESS = 0x82
+    USBTMC_STATUS_SPLIT_IN_PROGRESS = 0x83
+    USB488_STATUS_INTERRUPT_IN_BUSY = 0x20
+
+    USBTMC_REQUEST_INITIATE_ABORT_BULK_OUT = 1
+    USBTMC_REQUEST_CHECK_ABORT_BULK_OUT_STATUS = 2
+    USBTMC_REQUEST_INITIATE_ABORT_BULK_IN = 3
+    USBTMC_REQUEST_CHECK_ABORT_BULK_IN_STATUS = 4
+    USBTMC_REQUEST_INITIATE_CLEAR = 5
+    USBTMC_REQUEST_CHECK_CLEAR_STATUS = 6
+    USBTMC_REQUEST_GET_CAPABILITIES = 7
+    USBTMC_REQUEST_INDICATOR_PULSE = 64
+
+    USB488_READ_STATUS_BYTE = 128
+    USB488_REN_CONTROL = 160
+    USB488_GOTO_LOCAL = 161
+    USB488_LOCAL_LOCKOUT = 162
+
+    USBTMC_HEADER_SIZE = 12
+
+    RIGOL_QUIRK_PIDS = [0x04ce, 0x0588]
+
+    def list_devices():
+        "List all connected USBTMC devices"
+
+        def is_usbtmc_device(dev):
+            for cfg in dev:
+                d = usb.util.find_descriptor(cfg, bInterfaceClass=USBTMC_bInterfaceClass,
+                                             bInterfaceSubClass=USBTMC_bInterfaceSubClass)
+                if d is not None:
+                    return True
+
+                if dev.idVendor == 0x1334:
+                    # Advantest
+                    return True
+
+                if dev.idVendor == 0x0957:
+                    # Agilent
+                    if dev.idProduct in [0x2818, 0x4218, 0x4418]:
+                        # Agilent U27xx modular devices in firmware update mode
+                        # 0x2818 for U2701A/U2702A (firmware update mode on power up)
+                        # 0x4218 for U2722A (firmware update mode on power up)
+                        # 0x4418 for U2723A (firmware update mode on power up)
+                        return True
+
+            return False
+
+        print(list(usb.core.find(find_all=True, custom_match=is_usbtmc_device)))
+
+    list_devices()
 
 if __name__ == '__main__':
     main()
