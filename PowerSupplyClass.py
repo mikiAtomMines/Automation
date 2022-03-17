11ed961a8d85f523f07b6f79fb69c661b51cfac3
Sebastian Miki-Silva <miki@atommines.com>
2022-03-17 13:04:32 -0500

    Separated PowerSupply and SPD3303X classes. SPD3303X is a child class of PowerSupply.
    Added comments and TODOs
    
    Small changes to some setters and getters.
    
    Separated work for USBTMC in a different branch
    
    Added MAX_voltage_limit property

--- PowerSupplyClass.py
+++ PowerSupplyClass.py
@@ -4,69 +4,112 @@ Created on Fri Mar 11 12:53:39 2022
 @author: Sebastian Miki-Silva
 """
 
-# TODO: fix usbtmc power supply class.
-# TODO: write master class power
+# TODO: Add proper error handling
 
 import socket
-import usbtmc
-import usb
+
 import sys
 import time
 
 
 class PowerSupply:
-    """An ethernet-controlled power supply. Querys and commands based on manual for Siglent SPD3303X power supply."""
+    """A benchtop programmable power supply"""
+
+    def __init__(
+            self,
+            MAX_voltage_limit=None,
+            MAX_current_limit=None,
+            number_of_channels=1,
+            reset_channels=True,
+
+    ):
+        self._MAX_voltage_limit = MAX_voltage_limit
+        self._MAX_current_limit = MAX_current_limit
+        self._number_of_channels = number_of_channels
+        self._reset_channels = reset_channels
+
+    @property
+    def MAX_voltage_limit(self):
+        return self._MAX_voltage_limit
+
+    @property
+    def MAX_current_limit(self):
+        return self._MAX_current_limit
+
+    @property
+    def number_of_channels(self):
+        return self._number_of_channels
+
+    @property
+    def reset_channels(self):
+        return self._reset_channels
+
+
+class SPD3303X(PowerSupply):
+    """
+    An ethernet-controlled power supply. Querys and commands based on manual for Siglent SPD3303X power supply.
+    All voltages and currents are in Volts and Amps unless specified otherwise.
+    """
 
     def __init__(
             self,
             ip4_address=None,
             port=50000,
-            MAX_voltage_limit=1000,
-            MAX_current_limit=100,
-            ch1_voltage_limit=1000,
-            ch1_current_limit=100,
-            ch2_voltage_limit=1000,
-            ch2_current_limit=100,
+            ch1_voltage_limit=32,
+            ch1_current_limit=3.3,
+            ch2_voltage_limit=32,
+            ch2_current_limit=3.3,
             reset_channels=True
     ):
 
         """
-        Initialize a new ethernet power supply.
+        Initialize a new SPD3303X power supply.
 
         - param ip4_address: ------- IPv4 address of the power supply.
         - param port: -------------- port used for communication. Siglent recommends to use 5025 for the SPD3303X power
                                      supply. For other devices, can use any between 49152 and 65536.
-        - param MAX_voltage_limit: - Maximum voltage that the power supply can output.
-        - param MAX_current_limit: - Maximum current that the power supply can output.
+        - param MAX_voltage_limit: - Maximum voltage that the power supply can output based on hardware limitations.
+        - param MAX_current_limit: - Maximum current that the power supply can output based on hardware limitations.
         - param ch1_voltage_limit: - Set an upper limit on the voltage output of channel 1.
         - param ch1_current_limit: - Set an upper limit on the current output of channel 1.
         - param ch2_voltage_limit: - Set an upper limit on the voltage output of channel 2.
         - param ch2_current_limit: - Set an upper limit on the current output of channel 2.
         - param reset_channels: ---- If True, run a routine to set turn off the output of both channels and set the set
 
+
+        Note that all voltage limits are software-based by this program. The power supply and its firmware does not have
+        any feature for limiting voltage or current outputs.
         """
 
         self._ip4_address = ip4_address
         self._port = port
-        self._MAX_voltage_limit = MAX_voltage_limit
-        self._MAX_current_limit = MAX_current_limit
+
         self._ch1_voltage_limit = ch1_voltage_limit
         self._ch1_current_limit = ch1_current_limit
         self._ch2_voltage_limit = ch2_voltage_limit
         self._ch2_current_limit = ch2_current_limit
-        
+
+        self._number_of_channels = 2
+        self._MAX_voltage_limit = 32
+        self._MAX_current_limit = 3.3
+        self._reset_channels = True
+
         try:
             socket_ps = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             socket_ps.connect((self._ip4_address, self._port))
         except socket.error:
-            print('ERROR: Could not connect to power supply')
+            print('ERROR: Could not connect to power supply. Please Check IPv4 address and try again. ')
             sys.exit()
             
         self._socket = socket_ps
         
-        if reset_channels:
+        if reset_channels is True:
             self.reset_channels()
-        
+
+# =============================================================================
+#       Class methods
+# =============================================================================
+
     def _query(self, query):
         query_bytes = query.encode('utf-8')
         socket_ps = self._socket
@@ -97,7 +140,9 @@ class PowerSupply:
 # =============================================================================
 #       Get methods
 # =============================================================================    
-    
+
+# System =============================================================================
+
     @property
     def idn(self):
         qry = '*IDN?'
@@ -114,7 +159,9 @@ class PowerSupply:
         reply_hex_str = self._query(qry)   # bytes representing hex number
         reply_bin_str = f'{int(reply_hex_str, 16):0>10b}'  # binary num as string
         return reply_bin_str
-    
+
+# Channel properties =============================================================================
+
     @property
     def ch1_state(self):
         one_zero = int(self.system_status_binary[-5])
@@ -122,8 +169,8 @@ class PowerSupply:
     
     @property
     def ch2_state(self):
-        one_zero = int(self.system_status_binary[-6])
-        return bool(one_zero)
+        one_or_zero = int(self.system_status_binary[-6])
+        return bool(one_or_zero)
         
     @property
     def ch1_set_voltage(self):
@@ -135,9 +182,9 @@ class PowerSupply:
         qry = 'CH2:voltage?'
         return self._query(qry)
         
-    def get_set_voltage(self, channel):
-        qry = channel.upper() + ':voltage?'
-        return self._query(qry)
+    # def get_set_voltage(self, channel):  # TODO: Add format check for parameter channel
+    #     qry = channel.upper() + ':voltage?'
+    #     return self._query(qry)
     
     @property
     def ch1_actual_voltage(self):
@@ -149,9 +196,9 @@ class PowerSupply:
         qry = 'measure:voltage? CH2'
         return self._query(qry)
         
-    def get_actual_voltage(self, channel):
-        qry = 'measure:voltage? ' + channel.upper()
-        return self._query(qry)
+    # def get_actual_voltage(self, channel):  # TODO: Add format check for parameter channel
+    #     qry = 'measure:voltage? ' + channel.upper()
+    #     return self._query(qry)
         
     @property
     def ch1_set_current(self):
@@ -163,9 +210,9 @@ class PowerSupply:
         qry = 'CH2:current?'
         return self._query(qry)
         
-    def get_set_current(self, channel):
-        qry = channel.upper() + ':current?'
-        return self._query(qry)
+    # def get_set_current(self, channel):  # TODO: Add format check for parameter channel
+    #     qry = channel.upper() + ':current?'
+    #     return self._query(qry)
     
     @property
     def ch1_actual_current(self):
@@ -177,10 +224,12 @@ class PowerSupply:
         qry = 'measure:current? CH2'
         return self._query(qry)
         
-    def get_actual_current(self, channel):
-        qry = 'measure:current? ' + channel.upper()
-        return self._query(qry)
-    
+    # def get_actual_current(self, channel):  # TODO: Add format check for parameter channel
+    #     qry = 'measure:current? ' + channel.upper()
+    #     return self._query(qry)
+
+# channel limits =============================================================================
+
     @property     
     def ch1_voltage_limit(self):
         return self._ch1_voltage_limit
@@ -201,6 +250,8 @@ class PowerSupply:
 #       Set methods
 # =============================================================================
 
+# Channel properties =============================================================================
+
     @ch1_state.setter
     def ch1_state(self, state):
         cmd = 'Output CH1,' + state.upper()
@@ -213,31 +264,49 @@ class PowerSupply:
     
     @ch1_set_voltage.setter
     def ch1_set_voltage(self, volts):
-        cmd = 'CH1:voltage ' + str(volts)
-        self._command(cmd)
+        if volts <= self._ch1_voltage_limit:
+            cmd = 'CH1:voltage ' + str(volts)
+            self._command(cmd)
+        else:
+            print('CH1 voltage not set. \
+                  New voltage is higher than ch1 voltage limit.')
 
     @ch2_set_voltage.setter
     def ch2_set_voltage(self, volts):
-        cmd = 'CH2:voltage ' + str(volts)
-        self._command(cmd)
-    
-    def set_voltage(self, channel, volts):
-        cmd = channel.upper() + ':voltage ' + str(volts)
-        self._command(cmd)
+        if volts <= self._ch2_voltage_limit:
+            cmd = 'CH2:voltage ' + str(volts)
+            self._command(cmd)
+        else:
+            print('CH2 voltage not set. \
+                  New voltage is higher than ch1 voltage limit.')
+
+    # def set_voltage(self, channel, volts):  # TODO: Add voltage limiter based on the current channel's voltage limit
+    #     cmd = channel.upper() + ':voltage ' + str(volts)
+    #     self._command(cmd)
     
     @ch1_set_current.setter
     def ch1_set_current(self, amps):
-        cmd = 'CH1:current ' + str(amps)
-        self._command(cmd)
-    
+        if amps <= self._ch1_current_limit:
+            cmd = 'CH1:current ' + str(amps)
+            self._command(cmd)
+        else:
+            print('CH1 current not set. \
+                  New voltage is higher than ch1 voltage limit.')
+
     @ch2_set_current.setter
     def ch2_set_current(self, amps):
-        cmd = 'CH2:current ' + str(amps)
-        self._command(cmd)
+        if amps <= self._ch2_current_limit:
+            cmd = 'CH2:current ' + str(amps)
+            self._command(cmd)
+        else:
+            print('CH2 current not set. \
+                  New voltage is higher than ch1 voltage limit.')
     
-    def set_current(self, channel, amps):
-        cmd = channel.upper() + ':current ' + str(amps)
-        self._command(cmd)
+    # def set_current(self, channel, amps):  # TODO: Add current limiter based on the current channel's current limit
+    #     cmd = channel.upper() + ':current ' + str(amps)
+    #     self._command(cmd)
+
+# channel limits =============================================================================
 
     @ch1_voltage_limit.setter
     def ch1_voltage_limit(self, volts):
@@ -283,154 +352,62 @@ class PowerSupply:
         else:    
             self._ch2_current_limit = amps
 
-
-class UsbtmcPowerSupply:
-    def __init__(self, id_vendor=None, id_product=None, MAX_voltage_limit=1000, MAX_current_limit=100, reset_channels=True):
-        self._id_vendor = id_vendor
-        self._id_product = id_product
-        # self._MAX_voltage_limit = MAX_voltage_limit
-        # self._MAX_current_limit = MAX_current_limit
-        # self._ch1_voltage_limit = MAX_voltage_limit
-        # self._ch1_current_limit = MAX_current_limit
-        # self._ch2_voltage_limit = MAX_voltage_limit
-        # self._ch2_current_limit = MAX_current_limit
-
-        self._instrument = usbtmc.Instrument(id_vendor, id_product)
-
-    def _query(self, query):
-        reply = self._instrument.ask('*IDN?')
-        return reply
-
     
 def testing_EthernetPowerSupply():
 
-    SPD3303X = EthernetPowerSupply('10.176.42.121', 5025, MAX_voltage_limit=32, MAX_current_limit=3.2)
-    print(SPD3303X.idn)
-    print(SPD3303X.ip4_address)
-    print(SPD3303X.system_status_binary)
+    SPD3303X_1 = SPD3303X('10.176.42.121', 5025)
+    print(SPD3303X_1.idn)
+    print(SPD3303X_1.ip4_address)
+    print(SPD3303X_1.system_status_binary)
     print()
     
     input('Press enter to set ch1 to 3.45V, and ch2 to 1.51V')
-    SPD3303X.ch1_set_voltage=3.45
-    SPD3303X.ch2_set_voltage=1.51
-    print('CH1 and CH2 set voltages are:', SPD3303X.ch1_set_voltage, SPD3303X.ch2_set_voltage)
-    print('CH1 and CH2 set currents are:', SPD3303X.ch1_set_current, SPD3303X.ch2_set_current)
-    print('CH1 and CH2 actual voltages are:', SPD3303X.ch1_actual_voltage, SPD3303X.ch2_actual_voltage)
-    print('CH1 and CH2 actual currents are:', SPD3303X.ch1_actual_current, SPD3303X.ch2_actual_current)
+    SPD3303X_1.ch1_set_voltage=3.45
+    SPD3303X_1.ch2_set_voltage=1.51
+    print('CH1 and CH2 set voltages are:', SPD3303X_1.ch1_set_voltage, SPD3303X_1.ch2_set_voltage)
+    print('CH1 and CH2 set currents are:', SPD3303X_1.ch1_set_current, SPD3303X_1.ch2_set_current)
+    print('CH1 and CH2 actual voltages are:', SPD3303X_1.ch1_actual_voltage, SPD3303X_1.ch2_actual_voltage)
+    print('CH1 and CH2 actual currents are:', SPD3303X_1.ch1_actual_current, SPD3303X_1.ch2_actual_current)
     print()
     
     input('Press enter to turn ch1 on, wait 5 sec, then off. Repeat for ch2.')
-    SPD3303X.ch1_state='ON'
-    print('CH1 state is:', str(SPD3303X.ch1_state))
+    SPD3303X_1.ch1_state='ON'
+    print('CH1 state is:', str(SPD3303X_1.ch1_state))
     time.sleep(5)
-    SPD3303X.ch1_state='OFF'
-    print('CH1 state is:', str(SPD3303X.ch1_state))
-    SPD3303X.ch2_state='ON'
-    print('CH2 state is:', str(SPD3303X.ch2_state))
+    SPD3303X_1.ch1_state='OFF'
+    print('CH1 state is:', str(SPD3303X_1.ch1_state))
+    SPD3303X_1.ch2_state='ON'
+    print('CH2 state is:', str(SPD3303X_1.ch2_state))
     time.sleep(5)
-    SPD3303X.ch2_state='OFF'
-    print('CH2 state is:', str(SPD3303X.ch2_state))
+    SPD3303X_1.ch2_state='OFF'
+    print('CH2 state is:', str(SPD3303X_1.ch2_state))
     time.sleep(5)
     print()
     
     input('press enter to turn both on for 5 seconds. Then turn both off.')
-    SPD3303X.ch1_state='ON'
-    SPD3303X.ch2_state='ON'
+    SPD3303X_1.ch1_state='ON'
+    SPD3303X_1.ch2_state='ON'
     time.sleep(5)
-    print('CH1 and CH2 states are:', str(SPD3303X.ch1_state), str(SPD3303X.ch2_state))
-    print('CH1 and CH2 set voltages are:', SPD3303X.ch1_set_voltage, SPD3303X.ch2_set_voltage)
-    print('CH1 and CH2 set currents are:', SPD3303X.ch1_set_current, SPD3303X.ch2_set_current)
-    print('CH1 and CH2 actual voltages are:', SPD3303X.ch1_actual_voltage, SPD3303X.ch2_actual_voltage)
-    print('CH1 and CH2 actual currents are:', SPD3303X.ch1_actual_current, SPD3303X.ch2_actual_current)
+    print('CH1 and CH2 states are:', str(SPD3303X_1.ch1_state), str(SPD3303X_1.ch2_state))
+    print('CH1 and CH2 set voltages are:', SPD3303X_1.ch1_set_voltage, SPD3303X_1.ch2_set_voltage)
+    print('CH1 and CH2 set currents are:', SPD3303X_1.ch1_set_current, SPD3303X_1.ch2_set_current)
+    print('CH1 and CH2 actual voltages are:', SPD3303X_1.ch1_actual_voltage, SPD3303X_1.ch2_actual_voltage)
+    print('CH1 and CH2 actual currents are:', SPD3303X_1.ch1_actual_current, SPD3303X_1.ch2_actual_current)
     time.sleep(5)
-    SPD3303X.ch1_state='OFF'
-    SPD3303X.ch2_state='OFF'
-    print('CH1 and CH2 states are:', str(SPD3303X.ch1_state), str(SPD3303X.ch2_state))
-    print('CH1 and CH2 set voltages are:', SPD3303X.ch1_set_voltage, SPD3303X.ch2_set_voltage)
-    print('CH1 and CH2 set currents are:', SPD3303X.ch1_set_current, SPD3303X.ch2_set_current)
-    print('CH1 and CH2 actual voltages are:', SPD3303X.ch1_actual_voltage, SPD3303X.ch2_actual_voltage)
-    print('CH1 and CH2 actual currents are:', SPD3303X.ch1_actual_current, SPD3303X.ch2_actual_current)
+    SPD3303X_1.ch1_state='OFF'
+    SPD3303X_1.ch2_state='OFF'
+    print('CH1 and CH2 states are:', str(SPD3303X_1.ch1_state), str(SPD3303X_1.ch2_state))
+    print('CH1 and CH2 set voltages are:', SPD3303X_1.ch1_set_voltage, SPD3303X_1.ch2_set_voltage)
+    print('CH1 and CH2 set currents are:', SPD3303X_1.ch1_set_current, SPD3303X_1.ch2_set_current)
+    print('CH1 and CH2 actual voltages are:', SPD3303X_1.ch1_actual_voltage, SPD3303X_1.ch2_actual_voltage)
+    print('CH1 and CH2 actual currents are:', SPD3303X_1.ch1_actual_current, SPD3303X_1.ch2_actual_current)
     
-    SPD3303X.reset_channels()
-    SPD3303X.disconnect()
-
-
-def testing_UsbtmcPowerSupply():
-    SPD3303X = UsbtmcPowerSupply(id_vendor=0xF4EC, id_product=0x1430)
-
-
-def testing_usb_library():
-    USBTMC_bInterfaceClass = 0xFE
-    USBTMC_bInterfaceSubClass = 3
-    USBTMC_bInterfaceProtocol = 0
-    USB488_bInterfaceProtocol = 1
-
-    USBTMC_MSGID_DEV_DEP_MSG_OUT = 1
-    USBTMC_MSGID_REQUEST_DEV_DEP_MSG_IN = 2
-    USBTMC_MSGID_DEV_DEP_MSG_IN = 2
-    USBTMC_MSGID_VENDOR_SPECIFIC_OUT = 126
-    USBTMC_MSGID_REQUEST_VENDOR_SPECIFIC_IN = 127
-    USBTMC_MSGID_VENDOR_SPECIFIC_IN = 127
-    USB488_MSGID_TRIGGER = 128
-
-    USBTMC_STATUS_SUCCESS = 0x01
-    USBTMC_STATUS_PENDING = 0x02
-    USBTMC_STATUS_FAILED = 0x80
-    USBTMC_STATUS_TRANSFER_NOT_IN_PROGRESS = 0x81
-    USBTMC_STATUS_SPLIT_NOT_IN_PROGRESS = 0x82
-    USBTMC_STATUS_SPLIT_IN_PROGRESS = 0x83
-    USB488_STATUS_INTERRUPT_IN_BUSY = 0x20
-
-    USBTMC_REQUEST_INITIATE_ABORT_BULK_OUT = 1
-    USBTMC_REQUEST_CHECK_ABORT_BULK_OUT_STATUS = 2
-    USBTMC_REQUEST_INITIATE_ABORT_BULK_IN = 3
-    USBTMC_REQUEST_CHECK_ABORT_BULK_IN_STATUS = 4
-    USBTMC_REQUEST_INITIATE_CLEAR = 5
-    USBTMC_REQUEST_CHECK_CLEAR_STATUS = 6
-    USBTMC_REQUEST_GET_CAPABILITIES = 7
-    USBTMC_REQUEST_INDICATOR_PULSE = 64
-
-    USB488_READ_STATUS_BYTE = 128
-    USB488_REN_CONTROL = 160
-    USB488_GOTO_LOCAL = 161
-    USB488_LOCAL_LOCKOUT = 162
-
-    USBTMC_HEADER_SIZE = 12
-
-    RIGOL_QUIRK_PIDS = [0x04ce, 0x0588]
-
-    def list_devices():
-        "List all connected USBTMC devices"
-
-        def is_usbtmc_device(dev):
-            for cfg in dev:
-                d = usb.util.find_descriptor(cfg, bInterfaceClass=USBTMC_bInterfaceClass,
-                                             bInterfaceSubClass=USBTMC_bInterfaceSubClass)
-                if d is not None:
-                    return True
-
-                if dev.idVendor == 0x1334:
-                    # Advantest
-                    return True
-
-                if dev.idVendor == 0x0957:
-                    # Agilent
-                    if dev.idProduct in [0x2818, 0x4218, 0x4418]:
-                        # Agilent U27xx modular devices in firmware update mode
-                        # 0x2818 for U2701A/U2702A (firmware update mode on power up)
-                        # 0x4218 for U2722A (firmware update mode on power up)
-                        # 0x4418 for U2723A (firmware update mode on power up)
-                        return True
-
-            return False
-
-        print(list(usb.core.find(find_all=True, custom_match=is_usbtmc_device)))
-
-    list_devices()
+    SPD3303X_1.reset_channels()
+    SPD3303X_1.disconnect()
 
 
 def main():
-    testing_usb_library()
+
 
 if __name__ == '__main__':
     main()
