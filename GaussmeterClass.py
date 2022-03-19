56e25682a70fd35a525c1026b0c8d4d49ffe7a3a
Sebastian Miki-Silva <miki@atommines.com>
2022-03-18 20:00:46 -0500

    Testing write and read methods. Not working currently.

--- GaussmeterClass.py
+++ GaussmeterClass.py
@@ -8,23 +8,16 @@ import serial
 import sys
 import time
 
-# TODO: why does the read method take so long? about 20 seconds per byte. Why?
 
 def main():
     gaussmeter = serial.Serial(port='COM3', baudrate=115200, stopbits=1, parity=serial.PARITY_NONE)
     print(gaussmeter.name)
     print(gaussmeter.isOpen())
 
-    for i in range(6):
-        write = gaussmeter.write(b'ID_METER_PROP')
-        time.sleep(0.2)
+    w = gaussmeter.write(b'ID_METER_PROP')
+    r = gaussmeter.read()
 
-    out = ''
-
-
-    print('write:', write)
-    print('out:', out)
-
+    print(r)
 
     gaussmeter.close()
 
