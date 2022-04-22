d6a7a02977b417e3adc7b3872b358106628af1b4
Sebastian Miki-Silva <miki@atommines.com>
2022-04-21 20:43:27 -0500

    Added query and command methods to SocketEthernetDevice

--- connection_type.py
+++ connection_type.py
@@ -7,6 +7,7 @@ Created on Thursday, April 7, 2022
 # TODO: Finish adding comments
 
 import socket
+import time
 import sys
 
 
@@ -30,6 +31,51 @@ class SocketEthernetDevice:
 
         if ip4_address is not None:
             self.connect()
+
+    def _query(self, query):
+        """
+        send a query to the ethernet device and receive a response.
+
+        Parameters
+        ----------
+        query : bytes
+            Python bytes containing the query command. Dependent on each individual device.
+        Returns
+        -------
+        bytes
+            Returns the reply of the ethernet device as bytes.
+        """
+
+        try:
+            self._socket.sendall(query)
+            reply = self._socket.recv(4096)
+        except OSError:
+            raise OSError('ERROR: Socket not found. Query not sent. Try using the connect() method first.')
+
+        time.sleep(0.3)
+        return reply
+
+    def _command(self, cmd):
+        """
+        send a command to the ethernet device. Does not receive any response.
+
+        Parameters
+        ----------
+        cmd : bytes
+            Python btyes containing the command. Dependent on each individual device.
+        Returns
+        -------
+        None
+            Returns None if the command is succesfully sent.
+        """
+
+        try:
+            out = self._socket.sendall(cmd)
+        except OSError:
+            raise OSError('ERROR: Socket not found. Command not sent. Try using the connect() method first.')
+
+        time.sleep(0.3)
+        return out
 
     @property
     def ip4_address(self):
