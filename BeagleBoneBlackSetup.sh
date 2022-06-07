e5555c4a278ef3e3ea64f04405a347b5cf8f5e76
Sebastian Miki-Silva <miki@atommines.com>
2022-06-07 17:11:39 -0500

    finished all commands necessary for setup.
    User still needs to manually answer all prompts

--- BeagleBoneBlackSetup.sh
+++ BeagleBoneBlackSetup.sh
@@ -1,36 +1,53 @@
 # Sebastian Miki-Silva
 # June 7th, 2022
 
+cd ~/
 echo 'Updating sudo...'
-yes | sudo apt-get update
-yes | sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 printf '\n'
 
 
 echo 'Installing zip and unzip...'
-yes | sudo apt install zip
-yes | sudo apt install unzip
+sudo apt install zip
+sudo apt install unzip
 printf '\n'
 
 
 echo 'Intalling git...'
-yes | sudo apt-get install git git-core
+sudo apt-get install git git-core
 printf '\n'
 
 
 echo 'Installing Python3 libraries...'
-yes | pip3 install simple_pid
-yes | pip3 install pyserial
-yes | sudo apt-get install python3-matplotlib
+pip3 install simple_pid
+pip3 install pyserial
+sudo apt-get install python3-matplotlib
 printf '\n'
 
-
 echo 'Installing MCC Universal Library for Linux...'
-yes | sudo apt-get install gcc g++ make
-yes | sudo apt-get install libusb-1.0-0-dev
-yes | wget -N https://github.com/mccdaq/uldaq/releases/download/v1.2.1/libuldaq-1.2.1.tar.bz2
-yes | tar -xvjf libuldaq-1.2.1.tar.bz2
+sudo apt-get install gcc g++ make
+sudo apt-get install libusb-1.0-0-dev
+wget -N https://github.com/mccdaq/uldaq/releases/download/v1.2.1/libuldaq-1.2.1.tar.bz2
+tar -xvjf libuldaq-1.2.1.tar.bz2
 cd libuldaq-1.2.1
-yes | ./configure && make
-yes | sudo make install
-yes | pip3 install uldaq
+./configure && make
+sudo make install
+pip3 install uldaq
+cd ~/
+printf '\n'
+
+echo 'Downloading Automation library...'
+git clone https://github.com/mikiAtomMines/Automation.git
+cd Automation
+printf '\n'
+
+echo 'Finished'
+printf '\n'
+
+echo 'Unmounting USB drive...'
+sudo umount /media/usb1
+printf '\n'
+
+echo 'Safe to eject USB drive'
+echo 'Finished...'
