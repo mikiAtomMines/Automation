27d31a7fc00792f32f1e840065b767c5a7413108
Sebastian Miki-Silva <miki@atommines.com>
2022-06-07 21:24:44 -0500

    initial commit.

---
+++ bbb_setup_files/bbb_setup.sh
@@ -0,0 +1,13 @@
+<< comment
+
+Sebastian Miki-Silva
+miki@atommines.com
+
+Run this file right after flashing a BeagleBone Black rev C to set it up for use as a pid temperature controller with
+the Automation python library. Will install all required prerequisites. This was tested using Debian 10.3. Might not
+work as expected in other versions, depending on what comes preinstalled with the OS.
+
+comment
+
+
+./steps.sh < prompts.txt
